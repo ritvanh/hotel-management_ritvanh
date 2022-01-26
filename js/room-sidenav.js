c71632c4f000a1ca35734ee8181042967a29b922
RITVAN H <ritvan.hajdari@fshnstudent.info>
2022-01-26 09:35:18 +0100

    removed plain template

--- js/room-sidenav.js
+++
@@ -1,8 +0,0 @@
-function closeFilter(){
-    document.getElementById("sidenav1").style.display = "none";
-    document.getElementById("filterButton").style.display = "inline";
-}
-function showFilter(){
-    document.getElementById("filterButton").style.display = "none";
-    document.getElementById("sidenav1").style.display = "inline";
-}
